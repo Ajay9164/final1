168a6a511f1ddf43f49c564ea0df2869cd4f846f
Ajay T <ajayts23122000@gmail.com>
2024-12-18 15:52:36 +0530

    Removed deprecated Mongoose options useNewUrlParser and useUnifiedTopology

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ app.use(bodyParser.json());
 
 // MongoDB connection
 const MONGO_URI = process.env.MONGO_URI;
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true ,
-  serverSelectionTimeoutMS: 0, // Disable connection timeout
-  connectTimeoutMS: 0 // Disable connection timeout 
-  })
+mongoose.connect(MONGO_URI)
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("MongoDB connection error:", err));
 
